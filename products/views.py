47196ecd31ac2d95ede28014913ac90ded9d70a6
Chandni-L5 <qssinhux@students.codeinstitute.net>
2025-11-29 12:44:04 +0000

    add functionality to sort all products by price

--- products/views.py
+++ products/views.py
@@ -1,34 +1,64 @@
 from django.shortcuts import get_object_or_404, render, redirect, reverse
 from django.contrib import messages
-from django.db.models import Q
-from products.models import Category, Product
+from django.db.models import Q, Min
+from .models import Category, Product
 
 
-# Create your views here.
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
+
     products = Product.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
+    sortkey = None
 
     if request.GET:
+
+        # --- Sorting by price ---
+        if 'sort' in request.GET:
+            sort = request.GET['sort']
+            sortkey = sort
+
+            if sort == 'name':
+                sortkey = 'name'
+
+            elif sort == 'price':
+                products = products.annotate(min_price=Min('quantities__price'))
+                sortkey = 'min_price'
+
+        if 'direction' in request.GET and sortkey:
+            direction = request.GET['direction']
+            if direction == 'desc':
+                sortkey = f'-{sortkey}'
+
+        if sortkey:
+            products = products.order_by(sortkey)
+
+        # --- Category filtering ---
         if 'category' in request.GET:
             category_slugs = request.GET['category'].split(',')
             products = products.filter(category__slug__in=category_slugs)
             categories = Category.objects.filter(slug__in=category_slugs)
 
+        # --- Search bar function ---
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
+
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}' if sort and direction else ''
 
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
@@ -36,9 +66,11 @@ def all_products(request):
 
 def product_detail(request, product_id):
     """ A view to show individual product details """
-    product = get_object_or_404(Product, pk=product_id)
+    product = get_object_or_404(
+        Product.objects.prefetch_related('quantities__quantity'),
+        pk=product_id
+    )
     context = {
         'product': product,
     }
-
     return render(request, 'products/product_detail.html', context)
