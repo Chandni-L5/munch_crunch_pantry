a4e6c2ac833893c6b4fe103cc9083fc90f22fdc1
Chandni-L5 <qssinhux@students.codeinstitute.net>
2025-12-09 16:48:44 +0000

    prepare project for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn munch_crunch_pantry.wsgi:application
--- munch_crunch_pantry/settings.py
+++ munch_crunch_pantry/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 
 import os
+import dj_database_url
 
 if os.path.isfile('env.py'):
     import env
@@ -33,6 +34,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
+    'munch-crunch-pantry-c0989406ec70.herokuapp.com',
 ]
 
 
@@ -131,13 +133,19 @@ WSGI_APPLICATION = 'munch_crunch_pantry.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
