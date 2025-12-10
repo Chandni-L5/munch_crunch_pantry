718fdbe095fbf5b12aab0250cfc82316a317c42f
Chandni-L5 <qssinhux@students.codeinstitute.net>
2025-12-10 11:01:53 +0000

    load static for logo

--- munch_crunch_pantry/settings.py
+++ munch_crunch_pantry/settings.py
@@ -29,8 +29,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@=@8jjd0o6-yg!-$_@!6og^1#)u*wmx43d!y&)$)lky!++=2f-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+if 'DEVELOPMENT' in os.environ:
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
