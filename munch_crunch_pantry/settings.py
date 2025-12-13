c59cba482c541183a2caf481a0e88a1488076fa5
Chandni-L5 <qssinhux@students.codeinstitute.net>
2025-12-13 22:53:29 +0000

    set DEBUG to False for production security

--- munch_crunch_pantry/settings.py
+++ munch_crunch_pantry/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get(
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = os.environ.get("DEBUG", "False") == "True"
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
